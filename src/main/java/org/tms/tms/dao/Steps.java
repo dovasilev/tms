1204f026992f6ffc730321d3cb1ee6fa0e8ee76c
dmitriy <desalvo-uin@mail.ru>
2020-07-25 10:09:59 +0500

    забыл закомитить

--- src/main/java/org/tms/tms/dao/Steps.java
+++ src/main/java/org/tms/tms/dao/Steps.java
@@ -1,10 +1,15 @@
 package org.tms.tms.dao;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Steps {
-    private List<Steps> stepsList;
+
+    List<Step> steps;
 }
