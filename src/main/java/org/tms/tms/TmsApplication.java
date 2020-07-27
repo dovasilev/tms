051cd6e19669ed597c81c6911d6f8cefa53fb76e
dmitriy <desalvo-uin@mail.ru>
2020-07-27 22:24:17 +0500

    edit fronted to vaadin

--- src/main/java/org/tms/tms/TmsApplication.java
+++ src/main/java/org/tms/tms/TmsApplication.java
@@ -3,14 +3,15 @@ package org.tms.tms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"org.tms.tms.*"})
-@EntityScan("org.tms.tms.*")
-@EnableJpaRepositories("org.tms.tms.*")
-public class TmsApplication {
+@ComponentScan({"org.tms.tms*"})
+@EntityScan("org.tms.tms*")
+@EnableJpaRepositories("org.tms.tms*")
+public class TmsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TmsApplication.class, args);
